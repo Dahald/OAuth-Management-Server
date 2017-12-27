85d25725e52a0b138924d70559d56af5ec044516
DahDev <daahald3@gmail.com>
2017-12-27 20:25:03 +0100

    Clean imports.

--- OAuth-Server/src/main/java/pl/dahdev/oauthserver/config/OAuth2ServerConfig.java
+++ OAuth-Server/src/main/java/pl/dahdev/oauthserver/config/OAuth2ServerConfig.java
@@ -15,15 +15,12 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
-import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 
 import javax.sql.DataSource;
-import java.util.Arrays;
-import java.util.Collections;
 
 
 @Configuration
