cc4642a9b6c40afca3120ffe8d76e0fef416e90b
DahDev <daahald3@gmail.com>
2018-01-04 13:19:05 +0100

    Tests for RoleService have been added.

--- Management-App/src/test/java/pl/dahdev/managementapp/repository/RoleRepositoryTest.java
+++ Management-App/src/test/java/pl/dahdev/managementapp/repository/RoleRepositoryTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import pl.dahdev.managementapp.model.Role;
 
@@ -14,6 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class RoleRepositoryTest {
 
     private static final String ADMIN = "ROLE_ADMIN";
@@ -27,6 +29,15 @@ public class RoleRepositoryTest {
         roleRepository.save(role);
 
         Optional<Role> actualRole = roleRepository.findByRole(ADMIN);
+        assertThat(actualRole).hasValue(role);
+    }
+
+    @Test
+    public void whenGivenIdShouldReturnRole() {
+        Role role = new Role(ADMIN);
+        long id = roleRepository.save(role).getId();
+
+        Optional<Role> actualRole = roleRepository.findById(id);
         assertThat(actualRole).hasValue(role);
     }
 }
---
+++ Management-App/src/test/java/pl/dahdev/managementapp/service/RoleServiceTest.java
@@ -0,0 +1,57 @@
+package pl.dahdev.managementapp.service;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import pl.dahdev.managementapp.exception.RoleException;
+import pl.dahdev.managementapp.model.Role;
+import pl.dahdev.managementapp.repository.RoleRepository;
+import pl.dahdev.managementapp.service.impl.RoleServiceImpl;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class RoleServiceTest {
+
+    private static final String ADMIN = "ROLE_ADMIN";
+    private static final String USER = "ROLE_USER";
+    private static final long ID = 500;
+
+    private RoleService roleService;
+
+    @Mock
+    private RoleRepository roleRepository;
+
+    @Before
+    public void setupMock() {
+        MockitoAnnotations.initMocks(this);
+        this.roleService = new RoleServiceImpl(roleRepository);
+    }
+
+    @Test
+    public void testMockCreation() {
+        assertNotNull(roleRepository);
+    }
+
+    @Test
+    public void whenGivenRoleNameShouldReturnRole() throws RoleException {
+        Role role = new Role(ADMIN);
+        role.setId(ID);
+        when(roleRepository.findByRole(ADMIN)).thenReturn(Optional.of(role));
+        Role actualRole = roleService.findByRole(ADMIN);
+
+        verify(roleRepository).findByRole(ADMIN);
+        assertEquals(role, actualRole);
+    }
+
+    @Test(expected = RoleException.class)
+    public void whenGivenWrongRoleNameShouldThrownException() throws RoleException {
+        when(roleRepository.findByRole(USER)).thenReturn(Optional.empty());
+        roleService.findByRole(USER);
+    }
+}
